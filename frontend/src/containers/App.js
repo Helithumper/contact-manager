08d25811275196378f0dad6c6738b6eccb6125b6
Chris Feltner <chris.feltner@Knights.ucf.edu>
2019-09-18 20:51:25 -0400

    state management fix

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import Login from './Login';
+import ContactsList from '../components/ContactsList';
 
 function App() {
-  
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    userUuid: '',
+    sessionKey: ''
+  });
+
+  const handleChange = (name, changedValue) => {
+    setValues({...values, [name]: changedValue});
+    console.log(values);
+  };
 
   return (
     <div className="App">
-      <Login/>
+      <Login handleChange={handleChange} values={values}/>
     </div>
   );
 }
